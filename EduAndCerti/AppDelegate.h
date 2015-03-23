76541853346d4ca889a38d503d7517ebafcea1b4
SangChan Lee <sangchan82@gmail.com>
2015-03-23 15:56:43 +0900

    Initial Commit

---
+++ EduAndCerti/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EduAndCerti
+//
+//  Created by SangChan on 2015. 3. 23..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EduAndCerti/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  EduAndCerti
+//
+//  Created by SangChan on 2015. 3. 23..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
